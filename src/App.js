18ab0ebe6333e1ddb4c47421cd532b3492807ca3
ersin cakmak <ckmrsn@gmail.com>
2021-12-31 11:50:23 +0300

    Setup react app and build pages structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage, LandingPage, LoginPage, RegisterPage } from './pages'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
